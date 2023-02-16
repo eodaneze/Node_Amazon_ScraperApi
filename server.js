c06cc0c3b92e472667eed3ea28591cce024e3fb6
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-02-16 03:18:00 -0800

    making the api to be dynamic

--- server.js
+++ server.js
@@ -3,65 +3,78 @@ const request = require("request-promise");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-const apiKey = "b02a399e7d0cdafd8f6dfe43ba8d9e76"
-const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+// const apiKey = "b02a399e7d0cdafd8f6dfe43ba8d9e76"
+// const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
-app.use(express.json())
+const generateScraperUrl = (api_key) =>
+  `http://api.scraperapi.com?api_key=${api_key}&autoparse=true`;
+
+app.use(express.json());
 app.get("/", (req, res) => {
-     res.send("WELCOME TO THE AMAZON SCRAPPER API")
+  res.send("WELCOME TO THE AMAZON SCRAPPER API");
 });
 
 // get product details
-app.get("/product/:productid", async(req, res) => {
-     const{productid} = req.params
-     try{
-          const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productid}`);
-          res.json(JSON.parse(response));
-        //   the json.parse will make the result to be in json format
-     }
-     catch(err){
-         console.log(err);
-     }
-})
+app.get("/product/:productid", async (req, res) => {
+  const { productid } = req.params;
+  const { api_key } = req.query;
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key
+      )}&url=https://www.amazon.com/dp/${productid}`
+    );
+    res.json(JSON.parse(response));
+    //   the json.parse will make the result to be in json format
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // get product reviews
-app.get("/product/:productid/reviews", async(req, res) => {
-     const{productid} = req.params
-     try{
-          const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productid}`);
-          res.json(JSON.parse(response));
-        //   the json.parse will make the result to be in json format
-     }
-     catch(err){
-         console.log(err);
-     }
-})
+app.get("/product/:productid/reviews", async (req, res) => {
+  const { productid } = req.params;
+  const { api_key } = req.query;
+  try {
+    const response = await request(
+      `${generateScraperUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productid}`
+    );
+    res.json(JSON.parse(response));
+    //   the json.parse will make the result to be in json format
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // get product offers
-app.get("/product/:productid/offers", async(req, res) => {
-     const{productid} = req.params
-     try{
-          const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productid}`);
-          res.json(JSON.parse(response));
-        //   the json.parse will make the result to be in json format
-     }
-     catch(err){
-         console.log(err);
-     }
-})
+app.get("/product/:productid/offers", async (req, res) => {
+  const { productid } = req.params;
+  const{api_key} = req.query
+  try {
+    const response = await request(
+      `${generateScraperUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productid}`
+    );
+    res.json(JSON.parse(response));
+    //   the json.parse will make the result to be in json format
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // implementing a search functionality
-app.get("/search/:searchQuery", async(req, res) => {
-     const{searchQuery} = req.params
-     try{
-          const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`);
-          res.json(JSON.parse(response));
-        //   the json.parse will make the result to be in json format
-     }
-     catch(err){
-         console.log(err);
-     }
-})
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  const{api_key} = req.query
+  try {
+    const response = await request(
+      `${generateScraperUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+    res.json(JSON.parse(response));
+    //   the json.parse will make the result to be in json format
+  } catch (err) {
+    console.log(err);
+  }
+});
 app.listen(PORT, () => {
-    console.log(`server is running on localhost: ${PORT}`);
-})
+  console.log(`server is running on localhost: ${PORT}`);
+});
